0b2f0b114558b16c9c74f5c8f5420d0125a3577f
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-08 13:11:17 +0500

    done

--- projects/qrcode.rs
+++ projects/qrcode.rs
@@ -1,1 +1,64 @@
-fn main()
+// cargo.toml
+// [dependencies]
+// qrcode = "0.12.0"
+// image = "0.23.9"
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Image generation >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+use qrcode::QrCode;
+use image::Luma;
+
+fn image_generation(image: &str){
+    // Encode some data into string:
+    let code = QrCode::new(image).unwrap();
+
+    //render the &str into an image:
+    let image = code.render::<Luma<u8>>().build();
+
+    // save the image:
+    image.save("/tmp/qrcode2.png").unwrap();
+}
+
+fn main(){
+    image_generation("string generation");
+}
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<< String generation >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+use qrcode::QrCode;
+
+fn string_generation(string: &str){
+    let code = QrCode::new(string).unwrap();
+    let string = code.render::<char>()
+                 .quiet_zone(false)
+                 .module_dimensions(2, 1)
+                 .build();
+    println!("{}",string);
+}
+fn main(){
+    string_generation("hello");
+}
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<< SVG generation >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+use qrcode::{QrCode, Version, EcLevel};
+use qrcode::render::svg;
+fn main(){
+    let code = QrCode::with_version(b"01234567",Version::Micro(2) ,EcLevel::L).unwrap();
+    let image = code.render()
+                    .min_dimensions(200, 200)
+                    .dark_color(svg::Color("#800000"))
+                    .light_color(svg::Color("#ffff80"))
+                    .build();
+    println!("{}",image);
+}
+
+<<<<<<<<<<<<<<<<<<<<<<<<<<<<< Unicode string generation >>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+use qrcode::QrCode;
+use qrcode::render::unicode;
+
+fn main(){
+    let code = QrCode::new("Mow mow").unwrap();
+    let image = code.render::<unicode::Dense1x2>()
+                    .dark_color(unicode::Dense1x2::Light)
+                    .light_color(unicode::Dense1x2::Dark)
+                    .build();
+    println!("{}",image);
+}
