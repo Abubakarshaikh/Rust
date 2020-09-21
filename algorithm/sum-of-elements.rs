7b8dbd844bef494f4398c5dd35aacd7638eec895
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-09-21 14:22:47 +0500

    done

--- algorithm/sum-of-elements.rs
+++ algorithm/sum-of-elements.rs
@@ -1,9 +1,22 @@
-fn main(){
-    let elements = [5,15,20,25,30,35,40,45,50];
+// fn main(){
+//     let elements = [5,15,20,25,30,35,40,45,50];
 
-    let mut sum = 0;
-    for i in elements.iter(){
-        sum = sum + i;
+//     let mut sum = 0;
+//     for i in elements.iter(){
+//         sum = sum + i;
+//     }
+//     println!("{}",sum);
+// }
+
+
+fn main(){
+
+    let arr = ["a","b","u","b","a","k","a","r"];
+
+    let mut add = String::from("");
+    for i in arr.iter(){
+
+        add =  add + &i.to_string();
     }
-    println!("{}",sum);
+    println!("{}",add);
 }
