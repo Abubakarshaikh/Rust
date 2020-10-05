1611d239e605dbad3b5b52eec20dbf4944a7b0f3
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-10-05 19:34:55 +0500

    002-variable_scope.rs

--- book/002-default_variables_are_immutable.rs
+++
@@ -1,8 +0,0 @@
-fn main(){
-    let x = 5; // x, scop start
-    println!("The value of x is {}",x); //end
-    x = 6;
-
-    //cannot assign twice to immutable variable `x`
-    println!("The value of x is {}",x);
-}
