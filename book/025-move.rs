2d30816de2436111a4772d0a9c4687dab321c9b5
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-10-19 16:27:26 +0500

    done

--- book/025-move.rs
+++ book/025-move.rs
@@ -1,1 +1,12 @@
-move rs
+fn main(){
+
+
+    let data1 = "Hello, world"; // Stack:copy
+    let copyy = data1;
+    // println!("data1 is :{} and copyy is {}",data1,copyy);
+   
+    let data2 = String::from("Hello, world"); // Heap:move
+    let movee = data2;
+    println!("data2 is :{} and copyy is {}",data2,movee);
+
+}
