5482253c2135215d699830b7c33110c4c250440f
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-10-21 10:39:17 +0500

    done

--- book/031-structs.rs
+++ book/031-structs.rs
@@ -1,1 +1,3 @@
+// Structs let you create custom types that are meaningful for your domain
+// By using structs, you can keep associated pieces of data connected to each other and name each piece to make your code clear.
 031-structs.rs
