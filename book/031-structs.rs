0737b45c1c7e9762c7816a0dc5e4a8ff25b94cd7
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-10-25 12:05:10 +0500

    done

--- book/031-structs.rs
+++ book/031-structs.rs
@@ -1,3 +1,27 @@
 // Structs let you create custom types that are meaningful for your domain
 // By using structs, you can keep associated pieces of data connected to each other and name each piece to make your code clear.
-031-structs.rs
+struct UniAddmisstionForm{
+    name: String,
+    lastname: String,
+    age: i32,
+}
+
+fn main(){
+    let uniform_01 = UniAddmisstionForm{
+        name: String::from("Abubakar"),
+        lastname: String::from("Shaikh"),
+        age: 23,
+    };
+
+    let uniform_02 = UniAddmisstionForm{
+        name: String::from("Umar farroque"),
+        lastname: String::from("Shaikh"),
+        age: 19,
+    };
+
+    let uniform_03 = UniAddmisstionForm{
+        name: String::from("Umar farroque"),
+        lastname: uniform_02.lastname,
+        age: 19,
+    };
+}
