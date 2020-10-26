612000696370124ef6002fa72e4a9e250ec1786a
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-10-26 11:27:40 +0500

    done

--- book/033-method.rs
+++ book/033-method.rs
@@ -1,2 +1,20 @@
 // Methods let you specify the behavior that instances of your structs have
-033-method.rs
+struct Rectangle{
+    width: i32,
+    height: i32,
+}
+
+impl Rectangle{
+    fn area(&self) ->i32{
+        self.width * self.height
+    }
+}
+
+fn main(){
+    let rect1 = Rectangle{
+        width: 29,
+        height: 49,
+    };
+
+    println!("The area of rectangle is {} sqaure in pixel",rect1.area());
+}
