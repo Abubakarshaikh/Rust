58020af0ea2f2b2b3af267a0b0673b6d67648f97
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-10-27 11:46:37 +0500

    done

--- book/036-multiple-impl-blocks.rs
+++ book/036-multiple-impl-blocks.rs
@@ -1,1 +1,31 @@
-036-multiple-impl-blocks.rs
+// Multiple impl Blocks
+struct Rectangle {
+    width:u32,
+    height: u32,
+}
+
+impl Rectangle{
+    fn area(&self) ->u32{
+        self.width * self.height
+    }
+}
+impl Rectangle{
+    fn can_hold(&self, other: Rectangle) -> bool{
+        self.width > other.width && self.height > other.height
+    }
+}
+
+fn main(){
+    let rect1 = Rectangle{
+        width: 10,
+        height: 20,
+    };
+
+    let rect2 = Rectangle{
+        width: 30,
+        height: 40,
+    };
+
+    println!("{}",rect1.area());
+    println!("{:?}",rect1.can_hold(rect2));
+}
