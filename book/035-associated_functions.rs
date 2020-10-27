8cefcdd59a6b51a45876375fc9cfc819cbde76bc
Abubakar Shaikh <55477515+Abubakarshaikh@users.noreply.github.com>
2020-10-27 11:31:32 +0500

    done

--- book/035-associated_functions.rs
+++ book/035-associated_functions.rs
@@ -1,1 +1,21 @@
-035-associated_functions.rs
+// Associated Functions: Without &self paramenter
+#[derive(Debug)]
+struct Rectangle {
+    width:u32,
+    height:u32,
+}
+
+impl Rectangle{
+    fn square(size: u32)-> Rectangle{
+        Rectangle{
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main(){
+
+    let rect1 = Rectangle::square(3);
+    println!("{:?}",rect1);
+}
